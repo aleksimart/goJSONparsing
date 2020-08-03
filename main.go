a67748ab820cb62f618f4a50d28dca0a6a6bcebd
alek_martir <alekmartir@gmail.com>
2020-08-03 18:14:32 +0300

    Creating the original file

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
+package main
 
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+)
+
+type Person struct {
+    Name    string
+    Age     int
+}
+
+func personCreate(w http.ResponseWriter, r *http.Request) {
+   // Declare a new Person struct
+   var p Person
+
+   // Try to decode the request body into the struct. If there is an error,
+   // respond to the client with the error message and 400 status code
+   err := json.NewDecoder(r.Body).Decode(&p)
+   if err != nil {
+       http.Error(w, err.Error(), http.StatusBadRequest)
+       return
+   }
+
+   // Do something with the PErson struct...
+   fmt.Fprintf(w, "Person: %+v", p)
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/person/create", personCreate)
+
+    if err := http.ListenAndServe(":4000", mux); err != nil {
+        log.Fatal(err)
+    }
+}
